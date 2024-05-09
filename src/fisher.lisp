418646ad85932d2358a59d3709cc8b8057183cb7
Sibbe Bakker <sibbe.bakker@wur.nl>
2024-05-09 09:37:34 +0200

    Add assert statement

--- src/fisher.lisp
+++ src/fisher.lisp
@@ -1,9 +1,13 @@
+;; The Fisher test
+
 (defun factorial (n)
-  "Calculate the exact factorial of n"
-  ; n! = 1
+  "Calculate the factorial of n, known as n!"
+  ; 0! = 0
   (cond ((eql n 0) 1)
-        ; 0! = 1
-        ((eql n 1) 1)
         ; Here we calculate (n -1)! × n = n!
         (t (* n (factorial (- n 1))))))
 
+;; Test code
+(assert (eql (factorial 4) 24))
+(assert (eql (factorial 0) 1))
+(assert (eql (factorial 1) 1))
