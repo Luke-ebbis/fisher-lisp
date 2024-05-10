6476ba4b16fe2dade5b5e88826dbc392cc03c882
Sibbe Bakker <sibbe.bakker@wur.nl>
2024-05-10 12:48:39 +0200

    Fisher function implemented

--- src/fisher.lisp
+++ src/fisher.lisp
@@ -7,6 +7,22 @@
         ; Here we calculate (n -1)! × n = n!
         (t (* n (factorial (- n 1))))))
 
+(defun choose (n k)
+    "Binomial coeficient"
+    (/ (factorial n) 
+       ( * (factorial k)
+           (factorial (- n k)))))
+
+;; The Fisher function
+(defun fisher (a b c d)
+  "The Fisher exact test"
+  (let ((n (+ a b c d)))
+   (/ ( * (choose (+ a c) a)
+          (choose (+ b d) b)
+          (factorial (+ a b))
+          (factorial (+ c d)))
+     (factorial n))))
+
 ;; Test code
 (assert (eql (factorial 4) 24))
 (assert (eql (factorial 0) 1))
