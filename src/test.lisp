931ad1559468b095168a91a86df8d2eae775b80c
Sibbe Bakker <sibbe.bakker@wur.nl>
2024-05-11 19:46:37 +0200

    move tests to seperate code

---
+++ src/test.lisp
@@ -0,0 +1,19 @@
+(load "src/fisher.lisp")
+(declaim (sb-ext:muffle-conditions cl:warning))
+;; Test code
+(defun approx-equal (x y &optional (epsilon 1.0e-5))
+  "Test if two floats are approximately equal within a given epsilon.
+  Made with the help of Chatjippity"
+  (<= (abs (- x y)) epsilon))
+
+(assert (eql (factorial 4) 
+             24))
+(assert (eql (factorial 0) 
+             1))
+(assert (eql (factorial 1) 
+             1))
+(assert (approx-equal (float (fisher-p 1 9 11 3)) 0.001346076))
+
+(print "test sucessful!")
+(terpri)
+(exit)
